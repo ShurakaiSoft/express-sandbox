97382e0bc427fe30fbbf11f723cf39755284117a
Stephen Parker <ShurakaiSoft@gmail.com>
2013-07-07 22:53:12 +1000

    Added session routing

---
+++ routes/session.js
@@ -0,0 +1,28 @@
+/**
+ * session routes
+ */
+
+var users = require('../data/users');
+
+module.exports = function (app) {
+	
+	
+	
+	app.get('/session/new', function (req, res) {
+		res.render('session/new', { title: "Log in" });
+	});
+	
+	app.post('/session', function (req, res) {
+		if (users(req.body.username) && users[req.body.username].password === req.body.password) {
+			req.session.user = users[req.body.username];
+			res.redirect('/users');
+		} else {
+			res.redirect('/session/new');
+		}
+	});
+	
+	app.del('/session', function (req, res) {
+		req.session.destroy();
+		res.redirect('/users');
+	});
+};
