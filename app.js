347481eac091edc2f2c623438cd88ba06c288f6e
Stephen Parker <ShurakaiSoft@gmail.com>
2013-07-08 02:44:55 +1000

    Fixed session

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
-app.use(express.logger('dev'));
+app.use(express.logger('short'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(express.cookieParser("test"));
@@ -22,6 +22,13 @@ app.use(express.session({
 	secret: 'test',
 	maxAge: 3600000
 }));
+app.use(function (req, res, next) {
+	console.log(req.session.user);
+	res.locals.user = req.session.user;
+	next();
+});
+
+
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -32,6 +39,7 @@ if ('development' == app.get('env')) {
 
 require('./routes/index')(app);
 require('./routes/users')(app);
+require('./routes/session')(app);
 
 http.createServer(app).listen(app.get('port'), function () {
   console.log('Express server listening on port ' + app.get('port'));
--- routes/session.js
+++ routes/session.js
@@ -5,15 +5,13 @@
 var users = require('../data/users');
 
 module.exports = function (app) {
-	
-	
-	
+		
 	app.get('/session/new', function (req, res) {
 		res.render('session/new', { title: "Log in" });
 	});
 	
 	app.post('/session', function (req, res) {
-		if (users(req.body.username) && users[req.body.username].password === req.body.password) {
+		if (users[req.body.username] && users[req.body.username].password === req.body.password) {
 			req.session.user = users[req.body.username];
 			res.redirect('/users');
 		} else {
